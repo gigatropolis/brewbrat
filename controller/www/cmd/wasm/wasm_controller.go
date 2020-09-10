87540e1adb21ca6b69bf9bb0aef4c6efe130eb6e
Kregg Kemper <Kregg_Kemper@amat.com>
2020-09-09 18:31:22 -0700

    Playing with WEBASM

--- controller/www/cmd/wasm/wasm_controller.go
+++ controller/www/cmd/wasm/wasm_controller.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"syscall/js"
 )
 
-func prettyJson(input string) (string, error) {
+func prettyJSON(input string) (string, error) {
 	var raw interface{}
 	if err := json.Unmarshal([]byte(input), &raw); err != nil {
 		return "", err
@@ -33,7 +36,7 @@ func jsonWrapper() js.Func {
 		}
 		inputJSON := args[0].String()
 		fmt.Printf("input %s\n", inputJSON)
-		pretty, err := prettyJson(inputJSON)
+		pretty, err := prettyJSON(inputJSON)
 		if err != nil {
 			errStr := fmt.Sprintf("unable to parse JSON. Error %s occurred\n", err)
 			return errStr
@@ -45,8 +48,49 @@ func jsonWrapper() js.Func {
 	return jsonfunc
 }
 
+func postActor(name string, action string) error {
+	vals := url.Values{"Name": {srv}, "Event": {attr}}
+	resp, err := http.PostForm("http://127.0.0.1:8090/setactor/%s/%s", vals.Name[0], vals.Attr[0])
+
+	if err != nil {
+		fmt.Printf("server:%s POST error:%s\n", srv, err.Error())
+		continue
+	}
+	body, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		fmt.Printf("server:%s Read error:%s\n", srv, err2.Error())
+	}
+
+	if body == "ON" || body == "OFF" {
+		fmt.Printf("Status=%s", body)
+	} else {
+		fmt.Printf("Unknown response = %s", body)
+	}
+
+	resp.Body.Close()
+
+}
+func postActorWapper() js.Func {
+	actorFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "Invalid no of arguments passed"
+		}
+		inputActorName := args[0].String()
+		fmt.Printf("input %s\n", inputActorName)
+		err := postActor(inputActorName)
+		if err != nil {
+			errStr := fmt.Sprintf("unable to Post  Actor Value. Error %s occurred\n", err)
+			return errStr
+		}
+		return nil
+	})
+
+	return actorFunc
+}
+
 func main() {
 	fmt.Println("Go Web Assembly")
 	js.Global().Set("formatJSON", jsonWrapper())
+	js.Global().Set("UpdateRelayValue", postActorWapper())
 	<-make(chan bool)
 }
