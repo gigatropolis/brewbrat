05af8af1462eefa737bdd77006630ac99c5086f9
gigatropolis <gigatropolis@yahoo.com>
2020-10-23 16:02:43 -0700

    Equipment updates

--- controller/control/equipment.go
+++ controller/control/equipment.go
@@ -7,6 +7,17 @@ import (
 // Equipment messages
 const (
 	CmdUpdateDevices = iota + 1
+	CmdChangeState
+)
+
+const (
+	EqStateIdle = iota + 1
+	EqStateActive
+)
+
+const (
+	EqModePIDControl = iota + 1
+	EqModeHistorisis
 )
 
 type SensValue struct {
@@ -22,6 +33,12 @@ type ActValue struct {
 type EquipMessage struct {
 	DeviceName string
 	Cmd        int
+	FltParam1  float64
+	FltParam2  float64
+	IntParam1  int64
+	IntParam2  int64
+	StrParam1  string
+	StrParam2  string
 	Sensors    []SensValue
 	Actors     []ActValue
 }
@@ -37,6 +54,8 @@ type IEquipment interface {
 
 type Equipment struct {
 	Device
+	State   int
+	Mode    int
 	Sensors map[string]SensValue
 	Actors  map[string]ActValue
 	in      <-chan EquipMessage
@@ -51,7 +70,28 @@ func (eq *Equipment) InitEquipment(name string, logger *Logger, properties []Pro
 	eq.out = out
 	eq.Sensors = make(map[string]SensValue)
 	eq.Actors = make(map[string]ActValue)
+
+	props := eq.GetProperties()
+	mode := props.InitProperty("Control Mode", "string", "Historisis", "Control mode for equipment").(string)
+
+	switch mode {
+	case "Historisis":
+		eq.Mode = EqModeHistorisis
+	case "PID":
+		eq.Mode = EqModePIDControl
+	default:
+		eq.Mode = EqModeHistorisis
+	}
+
 	return nil
+}
+
+func (eq *Equipment) isValidState(state int64) bool {
+	if state != EqStateActive &&
+		state != EqStateIdle {
+		return false
+	}
+	return true
 }
 
 func (eq *Equipment) AddSensor(name string) error {
@@ -90,6 +130,10 @@ func (eq *Equipment) handleMessage(message EquipMessage) error {
 			a.Power = actor.Power
 			eq.Actors[actor.Name] = a
 		}
+	case CmdChangeState:
+		if eq.isValidState(message.IntParam1) {
+			eq.State = int(message.IntParam1)
+		}
 	}
 	return nil
 }
@@ -119,5 +163,14 @@ func (rim *SimpleRIMM) InitEquipment(name string, logger *Logger, properties []P
 }
 
 func (rim *SimpleRIMM) NextStep() error {
+
+	switch rim.State {
+	case EqStateActive:
+		rim.updateActors()
+	}
 	return nil
+}
+
+func (rim *SimpleRIMM) updateActors() error {
+
 }
